6382c9a4a24517926cca672fae63e1cb9772d7ed
BaeKoR <inser98@naver.com>
2023-03-13 19:54:47 +0900

    merge tiles

--- src/main/java/mul/cam/a/member/MemberController.java
+++ src/main/java/mul/cam/a/member/MemberController.java
@@ -1,7 +1,10 @@
 package mul.cam.a.member;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import mul.cam.a.service.MemberService;
 
@@ -10,6 +13,13 @@ public class MemberController {
 	// service 접근(생성)
 	@Autowired
 	MemberService service; // db는 무조건 service를 통한다
+	
+	@GetMapping(value = "mainpage.do")
+	public String main() {
+		System.out.println("MemberController mainpage " + new Date());
+		
+		return "mainpage";
+	}
 }
 
 
